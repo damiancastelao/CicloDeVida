815d782ecdb35e63f6d5f1f329b3bf7688b2edf8
Damian <damian@danielcastelao.org>
2021-04-12 10:24:18 +0200

    actualizado androidx

--- app/src/main/java/com/dam2/ciclodevida/MainActivity.kt
+++ app/src/main/java/com/dam2/ciclodevida/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.dam2.ciclodevida
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
